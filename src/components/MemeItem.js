c58b008e9550ba4492d604c2aa9e2873a7800041
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2017-08-18 17:06:07 +0300

    animation meme items

--- src/components/MemeItem.js
+++ src/components/MemeItem.js
@@ -2,17 +2,34 @@ import React, { Component } from 'react'
 
 
 class MemeItem extends Component {
+  constructor() {
+    super()
 
+    this.state = {
+      hovered: false
+    }
+    this.hideText = this.hideText.bind(this)
+  }
+
+  hideText() {
+    this.setState(prevState => ({
+      hovered: !prevState.hovered
+    }))
+  }
 
   render() {
     return (
-      <div className="meme-item">
+      <div
+        className="meme-item"
+        onMouseEnter={this.hideText}
+        onMouseLeave={this.hideText}
+      >
         <img
           src={this.props.meme.url}
           alt={this.props.meme.name}
-          className="meme-img"
+          className={this.state.hovered ? "meme-img darken-img" : "meme-img"}
         />
-        <p className="meme-txt">
+        <p className={this.state.hovered ? "meme-txt" : "no-txt"}>
           {this.props.meme.name}
         </p>
       </div>
